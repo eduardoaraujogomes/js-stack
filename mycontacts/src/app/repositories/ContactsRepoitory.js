edff00b5f2cfb79d7cb788171ec8f0efbae6e360
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-12-14 22:37:25 -0300

    Finished the API with a CRUD

---
+++ mycontacts/src/app/repositories/ContactsRepoitory.js
@@ -0,0 +1,93 @@
+const { v4 } = require('uuid');
+
+let contacts = [
+  {
+    // uuid -> Universal Unique ID - Gera uma HASH
+    id: v4(),
+    name: 'Mateus',
+    email: 'mateus@mail.com',
+    phone: '123414141',
+    category_id: v4(),
+
+  },
+  {
+
+    id: v4(),
+    name: 'Jose',
+    email: 'jose@mail.com',
+    phone: '12131414',
+    category_id: v4(),
+
+  },
+  {
+
+    id: v4(),
+    name: 'Maria',
+    email: 'maria@mail.com',
+    phone: '3131313155',
+    category_id: v4(),
+
+  },
+];
+
+class ContactsRepository {
+  findAll() {
+    return new Promise((resolve) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find(((contact) => contact.id === id)),
+    ));
+  }
+
+  findByEmail(email) {
+    return new Promise((resolve) => resolve(
+      contacts.find(((contact) => contact.email === email)),
+    ));
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
+  }
+
+  create({
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const newContact = {
+        id: v4(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+      contacts.push(newContact);
+      resolve(newContact);
+    });
+  }
+
+  update(id, {
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const updatedContact = {
+        id,
+        name,
+        email,
+        phone,
+        category_id,
+      };
+
+      contacts = contacts.map((contact) => (
+        contact.id === id ? updatedContact : contact));
+
+      resolve(updatedContact);
+    });
+  }
+}
+
+module.exports = new ContactsRepository();
