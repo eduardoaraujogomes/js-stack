c83e9d885702f0b2b1b4e5aa92442ad4a565dd19
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2022-01-18 09:59:00 -0300

    feat: props

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import Post from './Post';
 
-const category = 'Posts da semana';
 
-//Render -> Renderizar - Mostrar o componente na tela
+//Props(Propriedades) - São dados que um componente pai passa para um componente filho.
+//Props são apenas read only, ou seja, não podemos alterar o valor de uma propriedade.
+//A alteração tem que ser feita no componente pai.
+
 function App() {
   return (
     <>
-      <h1>JStack's Blog</h1>
-      <h2>{category}</h2>
+      <h1 >JStack's Blog</h1>
+      <h2>Posts da semana</h2>
+      <hr />
+      <Post
+        post={{
+          title: "Título da notícia 01",
+          subtitle: "Subtítulo da notícia 01"
+        }}
+      />
+      <Post
+        post={{
+          title: "Título da notícia 02",
+          subtitle: "Subtítulo da notícia 02"
+        }}
+      />
+      <Post
+        post={{
+          title: "Título da notícia 03",
+          subtitle: "Subtítulo da notícia 03"
+        }}
+      />
     </>
   );
 }
