33981680d5ce4bf8bd615443a5324649707e19a6
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2022-01-20 09:45:12 -0300

    feat: map in react

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -2,10 +2,25 @@ import React from 'react';
 import Header from './Header';
 import Post from './Post';
 
+const posts = [
+  {
+    title: 'Title#01', subtitle: 'Sub#01', likes: 20
+  },
+  {
+    title: 'Title#02', subtitle: 'Sub#02', likes: 10
+  },
+  {
+    title: 'Title#03', subtitle: 'Sub#03', likes: 50
+  },
+];
+
 
 //Props(Propriedades) - São dados que um componente pai passa para um componente filho.
 //Props são apenas read only, ou seja, não podemos alterar o valor de uma propriedade.
 //A alteração tem que ser feita no componente pai.
+
+//toda vez que a gente coloca um array dentro do jsx ele vai tentar pegar e por na tela
+// porém alguns formatos ele não sabe o que fazer e pode quebrar.
 
 function App() {
   return (
@@ -15,28 +30,18 @@ function App() {
       </Header>
 
       <hr />
-      <Post
-        likes={20}
-        post={{
-          title: "Título da notícia 01",
-          subtitle: "Subtítulo da notícia 01"
-        }}
-      />
-      <Post
-        likes={10}
-        post={{
-          title: "Título da notícia 02",
-          subtitle: "Subtítulo da notícia 02"
-        }}
-      />
-      <Post
-        likes={50}
-        post={{
-          title: "Título da notícia 03",
-          subtitle: "Subtítulo da notícia 03"
-        }}
-      />
+      {posts.map(post => (
+        <Post
+          key={post.title}
+          likes={post.likes}
+          post={{
+            title: post.title,
+            subtitle: post.subtitle
+          }}
+        />
+      ))}
     </>
+
   );
 }
 
