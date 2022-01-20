f7c665aef0a086f1b4d8e068483c2778afdb625d
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2022-01-20 10:29:12 -0300

    feat: setState and prevState

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,18 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Post from './Post';
-
-const posts = [
-  {
-    title: 'Title#01', subtitle: 'Sub#01', likes: 20
-  },
-  {
-    title: 'Title#02', subtitle: 'Sub#02', likes: 10
-  },
-  {
-    title: 'Title#03', subtitle: 'Sub#03', likes: 50
-  },
-];
 
 
 //Props(Propriedades) - São dados que um componente pai passa para um componente filho.
@@ -23,16 +11,56 @@ const posts = [
 // porém alguns formatos ele não sabe o que fazer e pode quebrar.
 
 function App() {
+
+  //Sempre que você quiser que o React renderize/Atualize algo em tela, você precisa de um estado.
+  //O estado é um objeto que armazena informações que podem ser alteradas.
+  //O React sabe que o estado mudou e vai renderizar novamente a tela.
+
+  const [posts, setPosts] = useState([
+    {
+      id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20
+    },
+    {
+      id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10
+    },
+    {
+      id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50
+    },
+    {
+      id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 50
+    },
+  ]);
+
+  //prevState é o estado anterior, ou seja, o estado anterior antes de atualizar o estado.
+  //prevState é utilizado, para evitar inconsistências no front-end.
+  //setState é a função que atualiza o estado.
+
+  function handleRefresh() {
+    setTimeout(() => {
+      setPosts((prevState) => [
+        ...prevState,
+        {
+          id: Math.random(),
+          title: `Title#0${prevState.length + 1}`,
+          subtitle: `Sub#0${prevState.length + 1}`,
+          likes: 50
+        }
+      ]);
+    }, 2000);
+  }
+
   return (
     <>
       <Header >
-        <h2>Posts da semana</h2>
+        <h2>Posts da semana
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header>
 
       <hr />
       {posts.map(post => (
         <Post
-          key={post.title}
+          key={post.id}
           likes={post.likes}
           post={{
             title: post.title,
@@ -43,6 +71,6 @@ function App() {
     </>
 
   );
-}
+};
 
 export default App;
