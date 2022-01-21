f21b57a1ef67e0f5375d729c718995ee84ba391a
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2022-01-21 09:47:02 -0300

    feat: conditional rendering

--- react-fundamentos/src/Post.js
+++ react-fundamentos/src/Post.js
@@ -2,14 +2,49 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 export default function Post(props) {
+
+  // pode fazer com if normal, com ternário ou short circuit evaluation, a renderização condicional
+
+  /*   if(props.post.read) {
+      return <h2>{props.post.title} já foi lido</h2>
+    }
+   */
+
+  // ternário
+
+  /*   return (
+      <>
+        {props.post.read
+          ? <h2>{props.post.title} já foi lido</h2>
+          : <>
+            <article>
+              <strong>
+                {props.post.read ? <s>{props.post.title}</s> : props.post.title}
+              </strong>
+              <button onClick={() => props.onRemove(props.post.id)}>Remover</button>
+              <br />
+              <small>{props.post.subtitle}</small><br />
+              Media: {props.post.likes / 2}
+            </article>
+            <br />
+          </>}
+      </>
+    ); */
+
+  //short circuit evaluation
+  //se for verdadeiro, retorna o da direita, senão, retorna o da esquerda
+
   return (
     <>
       <article>
-        <strong>{props.post.title}</strong>
+        <strong>
+          {props.post.read && <s>{props.post.title}</s>}
+          {!props.post.read && props.post.title}
+        </strong>
         <button onClick={() => props.onRemove(props.post.id)}>Remover</button>
         <br />
         <small>{props.post.subtitle}</small><br />
-        Media: {props.likes / 2}
+        Media: {props.post.likes / 2}
       </article>
       <br />
     </>
@@ -17,11 +52,12 @@ export default function Post(props) {
 }
 
 Post.propTypes = {
-  likes: PropTypes.number.isRequired,
   onRemove: PropTypes.func.isRequired,
   post: PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
-    subtitle: PropTypes.string.isRequired
+    likes: PropTypes.number.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    read: PropTypes.bool.isRequired,
   }).isRequired
 };
