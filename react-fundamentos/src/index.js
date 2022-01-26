6749146011d087bfbb117e2db267a37eef753b32
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2022-01-26 10:19:13 -0300

    refactor: components and styles

--- react-fundamentos/src/index.js
+++ react-fundamentos/src/index.js
@@ -1,19 +1,10 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-import GlobalStyle from './styles/global';
-import App from './components/App';
-
-
-
-
-
+import App from './App';
 
 ReactDOM.render(
-  <>
-    <GlobalStyle />
-    <App />
-  </>,
-  document.getElementById('root')
+  <App />,
+  document.getElementById('root'),
 );
